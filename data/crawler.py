c164a125da1762818d15f8187db0bb8a778c53c5
Krzysztof Bober <krzysztof.bober@inexto.com>
2023-08-24 14:33:20 +0200

    Bulletproofing and extending debug for Crawler

--- data/crawler.py
+++ data/crawler.py
@@ -33,60 +33,61 @@ class Crawler:
         return persisited_teams
 
     @classmethod
-    def persist_players(cls, test_mode: bool = False):
-        all_players = players.get_players()
-        if test_mode:
-            all_players = all_players[9]
-        Log.info(f"Persisting {len(all_players)} players.")
-        ignored_players, persisted_players = [], 0
-        for count, raw_player in enumerate(all_players):
-            api_player = ApiPlayer(player_id=raw_player.get('id'))
-            player = Player.create_from_api_model(api_player)
-            team = Team.fetch_by_id(player.player_id)
-            if team:
-                Log.info(f"\tPlayer #{count + 1}: {api_player.first_name} {api_player.last_name} ({team.name})")
-                player.persist()
-                persisted_players += 1
-            else:
-                Log.warning(f"No Team found for {api_player.first_name} {api_player.last_name}")
-                Log.warning("\tIgnoring.")
-                ignored_players.append(f"{player.first_name} {player.last_name}")
-        Log.info("______________ Done ______________")
-        Log.info(f"Persisted players: {persisted_players}")
-        Log.info(f"Ignored players count: {len(ignored_players)}")
-        Log.info(f"\t{', '.join(ignored_players)}")
-
-    @classmethod
-    def get_full_data_for_season(cls, season: Season, test_mode: bool = False):
+    def get_full_data_for_season(cls, season: Season, test_mode: bool = False, ignored_teams: [str] = None):
+        players_persisted, games_persisted, players_stats_persisted, teams_stats_persisted = 0, 0, 0, 0
+        games_ignored = []
+        ignored_teams = ignored_teams or []
         for team in Team.fetch_all():
+            if team.abbreviation in ignored_teams or None:
+                continue
             Log.info(f"\tTeam: {team.name}")
             season_games = ApiTeamGameStats.get_team_games(team_id=team.team_id, season=season)
             season_games = season_games if test_mode else season_games
             Log.info(f"\t{team.name} played {len(season_games)} games in season {season.name}")
             for index, team_game in enumerate(season_games):
                 api_game = ApiGame(game_id=team_game.get('GAME_ID'))
-                game = Game.create_from_api_model(api_game)
+                try:
+                    game = Game.create_from_api_model(api_game)
+                except (AttributeError, ValueError) as ex:
+                    Log.error(ex)
+                    games_ignored.append(api_game.game_id)
+                    continue
                 Log.info(f"\t\tGame #{index + 1}: {api_game.game_id} - {game.matchup}")
                 if not Game.fetch_by_id(game_id=api_game.game_id):
                     game.persist()
+                    games_persisted += 1
                 api_game_stats = ApiTeamGameStats(game_id=api_game.game_id, team_id=team.team_id)
                 tgs = TeamGameStats.create_from_api_model(api_game_stats)
                 tgs.persist()
+                teams_stats_persisted += 1
                 for player_id in api_game_stats.team_player_ids:
-                    api_player = ApiPlayer(player_id=player_id)
+                    try:
+                        api_player = ApiPlayer(player_id=player_id)
+                    except ValueError:
+                        Log.warning(f"Player {player_id} cant' be found. Ignoring")
+                        continue
                     Log.info(f"\t\t\tPlayer: {api_player.full_name}")
                     player = Player.create_from_api_model(api_player)
                     if not Player.fetch_by_id(player_id):
                         player.persist()
+                        players_persisted += 1
                     api_pst = ApiPlayerGameStats(game_id=api_game.game_id, player_id=player_id)
                     pgs = PlayerGameStats.create_from_api_model(api_pst)
                     if not PlayerGameStats.fetch(player_id=player_id, game_id=api_game.game_id):
                         pgs.persist()
+                        players_stats_persisted += 1
                 Log.info(f"\t\t----- Game {api_game.game_id} done -----")
             Log.info(f"\t----- Team {team.name} done -----")
         Log.info(f"----- Season {season.name} done -----")
+        Log.info(f"Players added: {players_persisted}")
+        Log.info(f"Games added: {games_persisted}")
+        Log.info(f"Games ignored ({len(games_ignored)}: {', '.join(games_ignored)}")
+        Log.info(f"Team stats added: {teams_stats_persisted}")
+        Log.info(f"Player stats added: {players_stats_persisted}")
 
 
 Database.recreate_database()
 Crawler.persist_teams()
-Crawler.get_full_data_for_season(season=Season(start_year=2022), test_mode=True)
+# ignore_teams = ['ATL', 'BOS', 'CLE', 'NOP', 'CHI', 'DAL', 'DEN', 'GSW', 'HOU']
+ignore_teams = None
+Crawler.get_full_data_for_season(season=Season(start_year=2022), test_mode=True, ignored_teams=ignore_teams)

9665fa06bd179953d7e20460e29bcb111d611858
Krzysztof Bober <krzysztof.bober@inexto.com>
2023-08-30 09:03:08 +0200

    Added Play-in Season type

--- nba_client/season_type.py
+++ nba_client/season_type.py
@@ -3,10 +3,12 @@ class SeasonType:
         '001': 'Pre Season',
         '002': 'Regular',
         '003': 'All-Star',
-        '004': 'Post Season'
+        '004': 'Post Season',
+        '005': 'Play In'
     }
 
     def __init__(self, season_id: str):
+        self.season_id = season_id
         self.name = self.SEASON_TYPES.get(season_id, 'Unknown')
 
     def __str__(self):

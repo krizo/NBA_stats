3ca7a876a5cafb29d1837af18b27efaa2f2bb451
Krzysztof Bober <krzysztof.bober@inexto.com>
2024-01-08 10:59:09 +0100

    Get player by kwargs

--- data/players_fetcher.py
+++ data/players_fetcher.py
@@ -1,10 +1,18 @@
+from pandas import DataFrame
+
 from data.base_fetcher import BaseFetcher
 
 
 class PlayersFetcher(BaseFetcher):
     _table_name = "players"
 
-
-
-
-
+    @classmethod
+    def get_by(cls, **kwargs) -> DataFrame:
+        query = f"SELECT * FROM Players WHERE "
+        params_counter = 0
+        for key, value in kwargs.items():
+            if params_counter > 0:
+                query += " AND "
+            query = query + f" {key} = '{value}'"
+            params_counter += 1
+        return cls._get_dataframe(query)
